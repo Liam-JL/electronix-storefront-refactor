b2470cbfb2a59a8c9a54198a734307ba37a682ca
LiamJL <paledeepened@gmail.com>
2026-02-18 15:12:27 +0000

    Set up router and outlet where pages will change

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import Landing from "./pages/Landing";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 
+//Pages
+import RootLayout from "./pages/RootLayout";
+import Landing from "./pages/Landing";
+import Store from "./pages/Store";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Landing />} />
+      <Route path="store" element={<Store />} />
+    </Route>,
+  ),
+);
 
 function App() {
   return (
-    <>
-      <Landing />
-    </>
+    <div className="h-screen">
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
