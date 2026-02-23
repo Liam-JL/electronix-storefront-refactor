5b40a9338aba6432762de104126bb6db2de309b1
LiamJL <paledeepened@gmail.com>
2026-02-23 14:51:49 +0000

    Add size variants for svg part of logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -10,9 +10,15 @@ function Logo({ size, variant }: LogoProps) {
   const textSizeClass =
     size === "large" ? "hidden md:block text-4xl" : "text-2xl";
 
+  const imgSizeClass = size === "large" ? "h-[40px] w-auto" : "h-[30px] w-auto";
+
   return (
     <div className="flex items-center gap-2">
-      <img src={`logoipsum-${variant}.svg`} alt="Electronix Logo" />
+      <img
+        src={`logoipsum-${variant}.svg`}
+        alt="Electronix Logo"
+        className={imgSizeClass}
+      />
       <span className={`audiowide-regular ${textSizeClass}`}>Electronix</span>
     </div>
   );
