1512dd52c614c0e496ffd4623ffc9064c56d4f53
LiamJL <paledeepened@gmail.com>
2026-02-24 15:48:27 +0000

    Add hero to Landing Page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,11 +1,29 @@
+import { Link } from "react-router";
+import { Navigate } from "react-router";
+import Button from "../components/Button";
 function Landing() {
   return (
     <div>
-      <h1 className="font-bold text-xl">Landing Page</h1>
+      <section className="hero">
+        <h1 className="orbitron font-medium">
+          Power Up Your Life with the Latest Tech
+        </h1>
+        <p>
+          Discover cutting-edge electronics, unbeatable prices, and fast
+          delivery - all in one place.
+        </p>
+        <Button
+          title="Shop Now"
+          onPress={() => {
+            console.log("button pressed");
+          }}
+          accessibilityLabel="Go to Shop"
+        />
+      </section>
     </div>
   );
 }
 
 export default Landing;
 
-// className="landing flex justify-center items-center h-screen"
+
