da7e0752e857153ba55c45451819a05896c1f428
LiamJL <paledeepened@gmail.com>
2026-02-24 13:05:40 +0000

    Create and style  Searchbar component and render conditionally in either nav or main depending on screen size

---
+++ src/components/Searchbar.tsx
@@ -0,0 +1,31 @@
+import { useNavigate } from "react-router";
+import { IoSearch as SearchIcon } from "react-icons/io5";
+
+function Searchbar() {
+  const navigate = useNavigate();
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const input = e.currentTarget.search as HTMLInputElement;
+    const searchTerm = input.value;
+    input.value = "";
+    navigate(`/store?search=${encodeURIComponent(searchTerm)}`);
+  }
+
+  return (
+    <form className="flex gap-2 w-full justify-center" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="search"
+        placeholder="Search products..."
+        autoComplete="off"
+        className="border-2 border-solid rounded-xl pl-4 pr-4 max-w-100 min-w-60"
+      />
+      <button type="submit" className="">
+        <SearchIcon className="w-6 h-auto" />
+      </button>
+    </form>
+  );
+}
+
+export default Searchbar;
