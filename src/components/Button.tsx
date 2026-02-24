db03e37906b273adbee96af202694e1140526c40
LiamJL <paledeepened@gmail.com>
2026-02-24 15:45:39 +0000

    Add transition to Button for hover and make component more resposnive.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,9 @@ type ButtonProps = {
   title: string;
   onPress: () => void;
   bgColor?: string;
+  bgHoverColor?: string;
   textColor?: string;
+  textHoverColor?: string;
   disabled?: boolean;
   accessibilityLabel?: string;
   className?: string;
@@ -12,7 +14,9 @@ function Button({
   title,
   onPress,
   bgColor = "bg-accent",
+  bgHoverColor = "hover:bg-accent-light",
   textColor = "text-electronix-white",
+  textHoverColor = "text-electronix-white",
   disabled = false,
   accessibilityLabel,
   className = "",
@@ -24,8 +28,8 @@ function Button({
       disabled={disabled}
       aria-label={accessibilityLabel}
       className={`
-        px-4 py-2 rounded-xl font-medium transition cursor-pointer
-        ${bgColor} ${textColor} 
+        px-4 py-2 rounded-xl font-medium cursor-pointer w-full transition-none md:transition-all duration-300 ease-in-out
+        ${bgColor} ${textColor} ${bgHoverColor} ${textHoverColor}
         disabled:opacity-50 disabled:cursor-not-allowed
         ${className}
       `}
