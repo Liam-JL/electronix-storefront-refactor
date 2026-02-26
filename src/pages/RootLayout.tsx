8b2bc2389ef0d18117bcd56b8d05f3aeb45d1b0b
LiamJL <paledeepened@gmail.com>
2026-02-26 11:50:30 +0000

    Add gradient and circuit board background to RootLayout for all Outlets to sit on

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -6,16 +6,17 @@ import Searchbar from "../components/Searchbar";
 function RootLayout() {
   return (
     <div className="flex flex-col h-screen">
-      <header>
+      <header className="bg-electronix-white shadow-md z-1">
         <Navbar />
       </header>
-      <main className="flex flex-col flex-1 p-8 justify-center items-center gap-8">
+      <main className="flex flex-col flex-1 p-8 justify-center items-center gap-8 bg-linear-to-br from-electronix-white to-electronix-light-grey z-0">
         <div className="md:hidden w-full">
           <Searchbar />
         </div>
         <Outlet />
+        <div className="page-background absolute bg-[url(circuit-board.svg)] h-full w-full -z-1 mask-[linear-gradient(to_bottom_right, transparent_50%,black_100%)] [-webkit-mask-image:linear-gradient(to_bottom_right,transparent_55%,black_100%)]"></div> 
       </main>
-      <footer>
+      <footer className="z-1">
         <Footer />
       </footer>
     </div>
