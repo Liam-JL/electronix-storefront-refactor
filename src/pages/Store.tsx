a7cc66a1a070b89bd610fa00020d06e20ba58d8f
LiamJL <paledeepened@gmail.com>
2026-02-26 14:48:42 +0000

    Layout Store and implement products loader

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,9 +1,33 @@
+import { useLoaderData } from "react-router";
+import { products } from "../data/products.json" assert { type: "json" };
+
 function Store() {
+  const products = useLoaderData();
+  
+
   return (
-    <div className="">
-      <h1 className="font-bold text-xl">Store</h1>
+    <div className="store">
+      <header>
+        <h1>Our Collection</h1>
+        <p>
+          Explore the latest electronics, hand-picked to power your everyday
+          life with style and performance.
+        </p>
+      </header>
+      <section>
+        <div className="filter-bar"></div>
+        <div className="product-grid">
+          {products.map(product => <p>{product.title}</p>)}
+        </div>
+      </section>
     </div>
   );
 }
 
 export default Store;
+
+//loader function
+export const productsLoader = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 800)); //emulate 800ms delay
+  return products;
+};
