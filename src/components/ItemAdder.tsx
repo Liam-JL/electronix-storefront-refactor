c54ef28db715a24d460fc013e0082c86aa661e00
Liam <liam.lirson@gmail.com>
2026-03-18 16:36:52 +0000

    Create ItemAdder component to move logic of adding items to Cart to one place.

---
+++ src/components/ItemAdder.tsx
@@ -0,0 +1,68 @@
+import Button from "./Button";
+import type { Product } from "../pages/Store";
+import { useState } from "react";
+
+type ItemAdderProps = {
+    product: Product
+}
+
+function ItemAdder({product}: ItemAdderProps ) {
+  const [currentQuantity, setCurrentQuantity] = useState(1);
+
+  function increaseQuantity() {
+    if (currentQuantity < 10) setCurrentQuantity(currentQuantity + 1);
+  }
+
+  function decreaseQuantity() {
+    if (currentQuantity === 1) return;
+    setCurrentQuantity(currentQuantity - 1);
+  }
+
+  return (
+    <div className="flex p-4 gap-4">
+      <div className="quantity-wrapper grid grid-cols-3 flex-1">
+        <label className="sr-only" htmlFor={`quantity-${product.id}`}>
+          {`Quantity for: ${product.title}`}
+        </label>
+
+        <Button
+          title="-"
+          onPress={decreaseQuantity}
+          defaultStyle={false}
+          accessibilityLabel="Decrease quantity"
+          className="bg-electronix-light-grey border border-electronix-grey hover:text-accent"
+        />
+
+        <input
+          type="number"
+          min={1}
+          value={currentQuantity}
+          id={`quantity-${product.id}`}
+          onChange={(e) => {
+            const value = Math.max(1, Math.min(10, Number(e.target.value)));
+            setCurrentQuantity(value);
+          }}
+          className="text-center [appearance:textfield] [&::-webkit-outer-spin-button]:!appearance-none [&::-webkit-inner-spin-button]:!appearance-none [&::-webkit-outer-spin-button]:!m-0 [&::-webkit-inner-spin-button]:!m-0"
+        />
+
+        <Button
+          title="+"
+          onPress={increaseQuantity}
+          defaultStyle={false}
+          accessibilityLabel="Increase quantity"
+          className="bg-electronix-light-grey border border-electronix-grey hover:text-accent"
+        />
+      </div>
+
+      <Button
+        title="Add"
+        onPress={() => {
+          console.log("Add button pressed");
+        }}
+        className="flex-1"
+      />
+    </div>
+  );
+}
+
+export default ItemAdder;
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,23 +1,12 @@
 import { Link } from "react-router";
-import { useState } from "react";
 import type { Product } from "../pages/Store";
-import Button from "./Button";
+import ItemAdder from "./ItemAdder";
 
 type ProductCardProps = {
   product: Product;
 };
 
 function ProductCard({ product }: ProductCardProps) {
-  const [currentQuantity, setCurrentQuantity] = useState(1);
-
-  function increaseQuantity() {
-    if (currentQuantity < 10) setCurrentQuantity(currentQuantity + 1);
-  }
-
-  function decreaseQuantity() {
-    if (currentQuantity === 1) return;
-    setCurrentQuantity(currentQuantity - 1);
-  }
 
   return (
     <article className="max-w-75 flex flex-col items-center border border-electronix-grey bg-electronix-white rounded-xl grow h-full md:hover:-translate-y-1 hover:shadow-xl  transition-all duration-200 ease-out">
@@ -38,44 +27,7 @@ function ProductCard({ product }: ProductCardProps) {
       </div>
 
       <footer className="flex p-4 border-t border-electronix-grey gap-4">
-        <div className="quantity-wrapper grid grid-cols-3 flex-1">
-          <label
-            className="sr-only"
-            htmlFor={`quantity-${product.id}`}
-          >{`Quantity for: ${product.title}`}</label>
-          <Button
-            title="-"
-            onPress={decreaseQuantity}
-            defaultStyle={false}
-            accessibilityLabel="Decrease quantity"
-            className="bg-electronix-light-grey border border-electronix-grey hover:text-accent"
-          />
-          <input
-            type="number"
-            min={1}
-            value={currentQuantity}
-            id={`quantity-${product.id}`}
-            onChange={(e) => {
-              const value = Math.max(1, Math.min(10, Number(e.target.value)));
-              setCurrentQuantity(value);
-            }}
-            className="text-center [appearance:textfield] [&::-webkit-outer-spin-button]:!appearance-none [&::-webkit-inner-spin-button]:!appearance-none [&::-webkit-outer-spin-button]:!m-0 [&::-webkit-inner-spin-button]:!m-0"
-          />
-          <Button
-            title="+"
-            onPress={increaseQuantity}
-            defaultStyle={false}
-            accessibilityLabel="Increase quantity"
-            className="bg-electronix-light-grey border border-electronix-grey hover:text-accent"
-          />
-        </div>
-        <Button
-          title="Add"
-          onPress={() => {
-            console.log("Add button pressed");
-          }}
-          className="flex-1"
-        />
+        <ItemAdder product={product}/>
       </footer>
     </article>
   );
