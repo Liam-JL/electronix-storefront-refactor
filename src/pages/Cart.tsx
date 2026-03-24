0c92c23128761a63a112d227e7f52185d5850bfe
Liam <liam.lirson@gmail.com>
2026-03-24 12:13:36 +0000

    Add message and cta button for hwen cart is empty.

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -4,8 +4,10 @@ import type { Product } from "./Store";
 import CartItemCard from "../components/CartItemCard";
 import { CiDeliveryTruck } from "react-icons/ci";
 import Button from "../components/Button";
+import { useNavigate } from "react-router";
 
 function Cart() {
+  const navigate = useNavigate();
   const { cartItems } = useCart();
   const products = useLoaderData() as Product[];
 
@@ -41,7 +43,9 @@ function Cart() {
         </ul>
       </section>
 
-      <aside className={`bg-electronix-white border border-electronix-grey rounded-xl p-4 flex flex-col h-fit ${cartItems.length < 1 && "hidden"}`}>
+      <aside
+        className={`bg-electronix-white border border-electronix-grey rounded-xl p-4 flex flex-col h-fit ${cartItems.length < 1 && "hidden"}`}
+      >
         <h2 className="text-center p-4">Order Summary</h2>
         <div className="border-t border-b p-4">
           <span className="flex justify-between">
@@ -63,6 +67,17 @@ function Cart() {
           onPress={() => console.log("checkout button pressed")}
         />
       </aside>
+
+      <div className={`empty-cart-message flex flex-col items-center gap-4 col-span-2 ${cartItems.length > 0 && "hidden"}`}>
+        <span>Your cart is currently empty.</span>
+        <Button
+          title="Continue Shopping"
+          onPress={() => {
+            navigate("/store");
+          }}
+          accessibilityLabel="Go to Store"
+        />
+      </div>
     </div>
   );
 }
