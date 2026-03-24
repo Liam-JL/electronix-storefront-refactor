3f04be4d6ed947f639b79c46a137cc1a5d01e3cb
Liam <liam.lirson@gmail.com>
2026-03-24 11:33:30 +0000

    Add grid for Cart page layout and make responsive.

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -17,8 +17,8 @@ function Cart() {
   const deliveryCost = cartSubtotal < 50 ? 5 : 0;
 
   return (
-    <div className="flex flex-col gap-4">
-      <header className="flex flex-col items-center">
+    <div className="grid gap-8">
+      <header className="flex flex-col items-center md:col-span-2">
         <h1>Cart</h1>
         <p className="flex justify-center items-center gap-4 text-center md:mb-8">
           <CiDeliveryTruck className="hidden md:inline" />
@@ -27,7 +27,7 @@ function Cart() {
         <CiDeliveryTruck className="md:hidden mb-4" />
       </header>
 
-      <section>
+      <section className="">
         <ul className="flex flex-col gap-4">
           {cartItems.map((item) => {
             const product = products.find((p) => p.id === item.id);
@@ -45,7 +45,7 @@ function Cart() {
         </ul>
       </section>
 
-      <aside className="bg-electronix-white border border-electronix-grey rounded-xl p-4 flex flex-col">
+      <aside className="bg-electronix-white border border-electronix-grey rounded-xl p-4 flex flex-col h-fit">
         <h2 className="text-center p-4" >Order Summary</h2>
         <div className="border-t border-b p-4">
 
