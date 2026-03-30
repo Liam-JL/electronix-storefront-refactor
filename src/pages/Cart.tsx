4de1a1f6374ce649bbf72b66aac27301ab409ddc
Liam <liam.lirson@gmail.com>
2026-03-30 16:14:53 +0100

    Fix alignment issues on cart page

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -30,8 +30,8 @@ function Cart() {
   const deliveryCost = cartSubtotal < 50 ? 5 : 0;
 
   return (
-    <div className="grid gap-8">
-      <header className="flex flex-col items-center md:col-span-2">
+    <div className="">
+      <header className="flex flex-col items-center">
         <h1>Cart</h1>
         <p className="flex justify-center items-center gap-4 text-center md:mb-8">
           <CiDeliveryTruck className="hidden md:inline" />
@@ -40,41 +40,41 @@ function Cart() {
         <CiDeliveryTruck className="md:hidden mb-4" />
       </header>
 
-      <section className="">
-        <ul className="flex flex-col gap-4">
-          {cartItems.map((item) => {
-            const product = products.find((p) => p.id === item.id);
-            if (!product) return null;
-            return (
-              <li key={item.id}>
-                <CartItemCard product={product} quantity={item.quantity} />
-              </li>
-            );
-          })}
-        </ul>
-      </section>
-
-      <aside
-        className={`bg-electronix-white border border-electronix-grey rounded-xl p-4 flex flex-col h-fit ${cartItems.length < 1 && "hidden"}`}
-      >
-        <h2 className="text-center p-4">Order Summary</h2>
-        <div className="border-t border-b p-4">
-          <span className="flex justify-between">
-            <span>Subtotal:</span>
-            <span>{`£${cartSubtotal.toFixed(2)}`}</span>
+      <div className="flex flex-col md:flex-row gap-8">
+        <section className="">
+          <ul className="flex flex-col gap-4">
+            {cartItems.map((item) => {
+              const product = products.find((p) => p.id === item.id);
+              if (!product) return null;
+              return (
+                <li key={item.id}>
+                  <CartItemCard product={product} quantity={item.quantity} />
+                </li>
+              );
+            })}
+          </ul>
+        </section>
+        <aside
+          className={`bg-electronix-white border border-electronix-grey rounded-xl p-4 flex flex-col h-fit ${cartItems.length < 1 && "hidden"}`}
+        >
+          <h2 className="text-center p-4">Order Summary</h2>
+          <div className="border-t border-b p-4">
+            <span className="flex justify-between">
+              <span>Subtotal:</span>
+              <span>{`£${cartSubtotal.toFixed(2)}`}</span>
+            </span>
+            <span className="flex justify-between">
+              <span>Delivery:</span>
+              <span>{`£${deliveryCost.toFixed(2)}`}</span>
+            </span>
+          </div>
+          <span className="flex justify-between p-4 text-2xl">
+            <span>Total:</span>
+            <span className="font-bold">{`£${(cartSubtotal + deliveryCost).toFixed(2)}`}</span>
           </span>
-
-          <span className="flex justify-between">
-            <span>Delivery:</span>
-            <span>{`£${deliveryCost.toFixed(2)}`}</span>
-          </span>
-        </div>
-        <span className="flex justify-between p-4 text-2xl">
-          <span>Total:</span>
-          <span className="font-bold">{`£${(cartSubtotal + deliveryCost).toFixed(2)}`}</span>
-        </span>
-        <Button title="Checkout" onPress={() => setOpenModal(true)} />
-      </aside>
+          <Button title="Checkout" onPress={() => setOpenModal(true)} />
+        </aside>
+      </div>
 
       <div
         className={`empty-cart-message flex flex-col items-center gap-4 col-span-2 ${cartItems.length > 0 && "hidden"}`}
