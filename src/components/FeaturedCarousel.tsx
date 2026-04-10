478b44b60e16dfab9b50326d8e19b6f98272d3c7
Liam <liam.lirson@gmail.com>
2026-04-10 16:48:36 +0100

    Add placement slot for price

--- src/components/FeaturedCarousel.tsx
+++ src/components/FeaturedCarousel.tsx
@@ -7,6 +7,8 @@ function FeaturedCarousel() {
       <div>
         <article>
           <img src="hero-image-placeholder.png" alt="" className="w-60 h-60" />
+          <h3 className="text-electronix-black">Example Product</h3>
+          {/* Put price here next */}
         </article>
       </div>
       <Button title={">"} onPress={() => {}} className="h-10" />
