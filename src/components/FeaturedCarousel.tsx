b02a36c53a9c6b8d1306b3fef2fdadb1060e893e
Liam <liam.lirson@gmail.com>
2026-04-14 11:16:58 +0100

    Make featured products cycle automatically

--- src/components/FeaturedCarousel.tsx
+++ src/components/FeaturedCarousel.tsx
@@ -1,6 +1,7 @@
 import Button from "./Button";
 import type { Product } from "../pages/Store";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router";
 
 type FeaturedCarouselProps = {
   featuredProducts: Product[];
@@ -10,41 +11,55 @@ function FeaturedCarousel({ featuredProducts }: FeaturedCarouselProps) {
   const [currentProduct, setCurrentProduct] = useState(0);
 
   function handleNextBtn() {
-    const card = document.getElementById(`product-card-${currentProduct}`);
-    if (currentProduct < featuredProducts.length - 1) {
-      setCurrentProduct(currentProduct + 1)
-    } else {
-      setCurrentProduct(0)
-    }
+    setCurrentProduct((prev) =>
+      prev < featuredProducts.length - 1 ? prev + 1 : 0,
+    );
   }
 
   function handlePrevBtn() {
-    const card = document.getElementById(`product-card-${currentProduct}`);
     if (currentProduct > 0) {
-      setCurrentProduct(currentProduct - 1)
+      setCurrentProduct(currentProduct - 1);
     } else {
-      setCurrentProduct(featuredProducts.length - 1)
+      setCurrentProduct(featuredProducts.length - 1);
     }
   }
+  useEffect(() => {
+    const interval = setInterval(handleNextBtn, 5000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   return (
-    <div className="w-full flex justify-center items-center gap-4">
+    <div className="w-full flex justify-center items-center gap-4 relative">
       <Button title={"<"} onPress={handlePrevBtn} className="h-10" />
-      <div>
+      <div className="p-1.5 rounded bg-black  text-white text-sm font-light absolute top-7 z-2 -translateX-[50%]">
+        Featured
+      </div>
+      <div className="bg-gray-300 p-4 rounded relative">
         {featuredProducts.map((product, i) => (
-          <article
-            key={i}
-            id={`product-card-${i}`}
-            className={i === currentProduct ? "block" : "hidden"}
-          >
-            <img
-              src={product.thumbnail}
-              alt={`Image of ${product.title}`}
-              className="w-60 h-60"
-            />
-            <h2 className="text-electronix-black">{product.title}</h2>
-            <span>{`£${product.price}`}</span>
-          </article>
+          <Link to={`/store/:${product.id}`}>
+            <article
+              key={i}
+              id={`product-card-${i}`}
+              className={`${i === currentProduct ? "block" : "hidden"} bg-electronix-white p-4 flex flex-col justify-between gap-2 min-h-[400px]`}
+            >
+              <div className="">
+                <img
+                  src={product.thumbnail}
+                  alt={`Image of ${product.title}`}
+                  className="object-cover h-fit w-fit"
+                />
+              </div>
+              <section className="flex flex-col gap-4">
+                <h3 className="text-electronix-black text-xl text-gray-600 font-extralight">
+                  {product.title}
+                </h3>
+                <span className="text-accent font-extralight border border-accent rounded-2xl p-1.5 text-sm">{`£${product.price}`}</span>
+              </section>
+            </article>
+          </Link>
         ))}
       </div>
       <Button title={">"} onPress={handleNextBtn} className="h-10" />
